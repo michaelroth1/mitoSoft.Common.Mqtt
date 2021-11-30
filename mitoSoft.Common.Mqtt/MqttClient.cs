5428ad49ef81a8a94c6e9c3c87340981bbb04409
Michael <michael.mr.roth@live.com>
2021-12-01 00:15:24 +0100

    initial

---
+++ mitoSoft.Common.Mqtt/MqttClient.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace mitoSoft.homeNet.Core.Mqtt
+{
+    public class MqttClient
+    {
+        public event Action<MqttMessage> MessageReceived;
+
+        private string _clientId;
+
+        private uPLibrary.Networking.M2Mqtt.MqttClient _client;
+
+        public MqttClient(string hostName)
+        {
+            this.HostName = hostName;
+        }
+
+        public string HostName { get; set; }
+
+        public List<string> Topics { get; set; }
+
+        public bool IsConnected => _client?.IsConnected ?? false;
+
+        public bool Connect(string clientId)
+        {
+            try
+            {
+                _clientId = clientId;
+                _client = new uPLibrary.Networking.M2Mqtt.MqttClient(this.HostName);
+
+                if (this.Topics != null && this.Topics.Count > 0)
+                {
+                    var qos = new List<byte>();
+
+                    foreach (var topic in this.Topics)
+                    {
+                        qos.Add(2);
+                    }
+
+                    _client.Subscribe(this.Topics.ToArray(), qos.ToArray());
+
+                    _client.MqttMsgPublishReceived += this.OnMessageReceived;
+                }
+
+                _client.ConnectionClosed += this.OnConnectionClosed;
+
+                _client.Connect(_clientId);
+
+                return true;
+            }
+            catch
+            {
+                this.Disconnect();
+
+                Task.Run(() => this.Reconnect());
+
+                return false;
+            }
+        }
+
+        public void Disconnect()
+        {
+            if (_client == null)
+            {
+                return;
+            }
+
+            this.UnregisterEvents();
+
+            if (!this.IsConnected)
+            {
+                return;
+            }
+
+            this._client.Disconnect();
+        }
+
+        private void UnregisterEvents()
+        {
+            try
+            {
+                _client.MqttMsgPublishReceived -= this.OnMessageReceived;
+            }
+            catch { }
+            try
+            {
+                _client.ConnectionClosed -= this.OnConnectionClosed;
+            }
+            catch { }
+        }
+
+        private void OnMessageReceived(object sender, uPLibrary.Networking.M2Mqtt.Messages.MqttMsgPublishEventArgs e)
+        {
+            var message = System.Text.Encoding.ASCII.GetString(e.Message);
+
+            MessageReceived?.Invoke(new MqttMessage(e.Topic, message));
+        }
+
+        public void Publish(string topic, string message, int qosLevel, bool retain)
+        {
+            if (this.IsConnected)
+            {
+                this._client.Publish(topic, System.Text.Encoding.ASCII.GetBytes(message), byte.Parse(qosLevel.ToString()), retain);
+            }
+        }
+
+        private void OnConnectionClosed(object sender, System.EventArgs e)
+        {
+            this.Disconnect();
+
+            Task.Run(() => this.Reconnect());
+        }
+
+        private void Reconnect()
+        {
+            Task.Delay(5000);
+
+            if (!_client.IsConnected)
+            {
+                this.Connect(_clientId);
+            }
+        }
+    }
+}
---
+++ mitoSoft.Common.Mqtt/MqttMessage.cs
@@ -0,0 +1,20 @@
+﻿namespace mitoSoft.homeNet.Core.Mqtt
+{
+    public class MqttMessage
+    {
+        public MqttMessage(string topic, string message)
+        {
+            this.Topic = topic;
+            this.Message = message;
+        }
+
+        public string Topic { get; set; }
+
+        public string Message { get; set; }
+
+        public override string ToString()
+        {
+            return this.Topic + ">" + this.Message;
+        }
+    }
+}
